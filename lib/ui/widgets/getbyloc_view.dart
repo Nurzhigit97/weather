aea21e83f3d7d8d8e16da2b8b44378ea9ab99f23
Nurzhigit <shaidievn@gmail.com>
2022-12-05 23:38:44 +0600

    add get current weather by Location

--- lib/ui/widgets/getbyloc_view.dart
+++ lib/ui/widgets/getbyloc_view.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_api/blocs/weather_fetch_cubit.dart';
+import 'package:weather_api/resources/location.dart';
 
 class GetByLocationView extends StatelessWidget {
   GetByLocationView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final weatherCubit = context.read<WeatherFetchCubit>();
+    final location = Location();
     return IconButton(
-        onPressed: () async {}, icon: Icon(Icons.location_on_outlined));
+        onPressed: () async {
+          final currentLocation = await location.getCorrentLocation();
+          final lat = await currentLocation.latitude;
+          final long = await currentLocation.longitude;
+          final resLocation = '$lat,$long';
+          weatherCubit.fetchWeatherByLocation(resLocation);
+        },
+        icon: Icon(Icons.location_on_outlined));
   }
 }
